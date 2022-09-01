a6f729138f566687048219a7c1635ae493e59323
James Kanin <kaninnoothep@gmail.com>
2022-09-01 21:38:34 +0700

    add default setIsCartOpen to CartContext

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,6 +2,7 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext({
   isCartOpen: false,
+  setIsCartOpen: () => {},
 });
 
 export const CartProvider = ({ children }) => {
