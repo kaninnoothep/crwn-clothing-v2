5963f42f0e9791cb3c4e7f61af0be7add577d256
James Kanin <kaninnoothep@gmail.com>
2022-09-02 18:43:29 +0700

    category preview with 4 column

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,6 @@
-import { useContext, Fragment } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useContext } from "react";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
 import { CategoriesContext } from "../../contexts/categories.context";
 
 import "./shop.styles.scss";
@@ -7,18 +8,13 @@ import "./shop.styles.scss";
 const Shop = () => {
   const { categoriesMap } = useContext(CategoriesContext);
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((key) => {
+        const product = categoriesMap[key];
+
+        return <CategoryPreview key={key} title={key} products={product} />;
+      })}
+    </div>
   );
 };
 
